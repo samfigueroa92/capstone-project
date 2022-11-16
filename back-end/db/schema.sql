5362af55c70db70584694e503d4b81bc3adaa6c9
Tom LaTulipe <tlatulipe@gmail.com>
2022-11-16 17:53:24 -0500

    Update schema.sql

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -5,42 +5,42 @@ CREATE DATABASE task_capstone;
 
 CREATE TABLE users_elderly (
     id SERIAL PRIMARY KEY, 
-    firstname TEXT,
-    lastname TEXT,
-    dob DATE,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    dob DATE NOT NULL,
     languages TEXT,
-    address TEXT,
+    address TEXT NOT NULL,
     unit TEXT,
-    city TEXT,
-    zipcode VARCHAR(255),
-    phonenumber VARCHAR(255),
-    email TEXT,
-    verified BOOLEAN,
+    city TEXT NOT NULL,
+    zipcode VARCHAR(255) NOT NULL,
+    phonenumber VARCHAR(255) NOT NULL,
+    email TEXT NOT NULL,
+    verified BOOLEAN default false,
     profilephoto TEXT
 );
 
 CREATE TABLE users_volunteers (
     id SERIAL PRIMARY KEY,
-    firstname TEXT,
-    lastname TEXT,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
     languages TEXT,
-    address TEXT,
+    address TEXT NOT NULL,
     unit TEXT,
-    city TEXT,
-    zipcode VARCHAR(255),
-    phonenumber VARCHAR(255),
-    email TEXT,
-    verified BOOLEAN,
+    city TEXT NOT NULL,
+    zipcode VARCHAR(255) NOT NULL,
+    phonenumber VARCHAR(255) NOT NULL,
+    email TEXT NOT NULL,
+    verified BOOLEAN default false,
     profilephoto TEXT
 );
 
 CREATE TABLE requests (
     id SERIAL PRIMARY KEY,
     elder INT references users_elderly(id),
-    description TEXT,
-    req_date DATE,
+    description TEXT NOT NULL,
+    req_date DATE NOT NULL,
     volunteer INT references users_volunteers(id),
-    location TEXT,
+    location TEXT NOT NULL,
     time TEXT,
     assigned BOOLEAN DEFAULT false
 ),
@@ -48,7 +48,7 @@ CREATE TABLE requests (
 CREATE TABLE reviews_elder (
     id SERIAL PRIMARY KEY,
     volunteer INT references users_volunteers(id),
-    description TEXT,
+    description TEXT NOT NULL,
     post_date DATE,
     request_id INT references requests(id)
 ),
@@ -56,19 +56,19 @@ CREATE TABLE reviews_elder (
 CREATE TABLE reviews_volunteer (
     id SERIAL PRIMARY KEY,
     elder INT references users_elderly(id),
-    description TEXT,
+    description TEXT NOT NULL,
     post_date DATE,
     request_id INT references requests(id)
 ),
 
 CREATE TABLE ratings_elder (
     id SERIAL PRIMARY KEY,
-    rating INT,
+    rating INT NOT NULL,
     request_id INT references users_elderly(id)
 ),
 
 CREATE TABLE ratings_volunteer (
     id SERIAL PRIMARY KEY,
-    rating INT,
+    rating INT NOT NULL,
     request_id INT references users_volunteers(id)
 )
