c00a9be4616ac12acbd2848b34aa33141951f6ee
Tom LaTulipe <tlatulipe@gmail.com>
2022-11-19 18:33:01 -0500

    get single request

--- back-end/queries/request.js
+++ back-end/queries/request.js
@@ -1,29 +1,29 @@
 const db = require("../db/dbConfig.js");
 
-
 // Index --- all posted requests *** This should only show OPEN requests ***
 const getAllRequests = async () => {
-    try {
-        console.log("Listing all posted requests");
-        const requests = await db.any("SELECT * FROM requests ORDER BY req_date ASC") // Lists requests by their date, ascending
-        return requests;        
-    } catch (error) {
-        return error;
-    }
-}
+  try {
+    console.log("Listing all posted requests");
+    const requests = await db.any(
+      "SELECT * FROM requests ORDER BY req_date ASC"
+    ); // Lists requests by their date, ascending
+    return requests;
+  } catch (error) {
+    return error;
+  }
+};
 
 // --Routes--
 
 //Single request
-const getRequest = async(id) => {
-    try {
-        console.log("Retreiving request from request table")
-    }
-}
-
-
-
-
-
+const getRequest = async (id) => {
+  try {
+    console.log("Retreiving request from request table");
+    const req = await db.one("SELECT * FROM requests WHERE id=$1", id);
+    return req;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {};
