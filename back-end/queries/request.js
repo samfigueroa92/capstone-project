9846bd46d723856f80fa310ce487fec725f2f896
Tom LaTulipe <tlatulipe@gmail.com>
2022-11-19 18:13:59 -0500

    basic users queries complete

--- back-end/queries/request.js
+++ back-end/queries/request.js
@@ -1,6 +1,18 @@
 const db = require("../db/dbConfig.js");
 
 
+// Index --- all posted requests *** This should only show OPEN requests ***
+const getAllRequests = async () => {
+    try {
+        console.log("Listing all posted requests");
+        const requests = await db.any("SELECT * FROM requests ORDER BY req_date ASC") // Lists requests by their date, ascending
+        return requests;        
+    } catch (error) {
+        return error;
+    }
+}
+
+
 
 
 
--- back-end/queries/users.js
+++ back-end/queries/users.js
@@ -1,31 +1,39 @@
 const db = require("../db/dbConfig.js");
 
 const getAllUsers = async () => {
-    try {
-        console.log("Listing all users from the users table");
-        const users = await db.any("SELECT * FROM users ORDER BY id ASC");
-        return users;
-    } catch (error) {
-        return error;
-    }
+  try {
+    console.log("Listing all users from the users table");
+    const users = await db.any("SELECT * FROM users ORDER BY id ASC");
+    return users;
+  } catch (error) {
+    return error;
+  }
 };
 
 // --Routes--
 
 //Single user
-const getUser = async(id) => {
-    try {
-        console.log("Retreiving user from table users");
-        const user = await db.one("SELECT * FROM users WHERE id=$1", id);
-        return user;
-    } catch (error) {
-        return error;
-    }
+const getUser = async (id) => {
+  try {
+    console.log("Retreiving user from table users");
+    const user = await db.one("SELECT * FROM users WHERE id=$1", id);
+    return user;
+  } catch (error) {
+    return error;
+  }
 };
 
+//Create user (backburner while we learn how the hell firebase works)
 
-
-
-
+//Delete user - requires admin auth
+const deleteUser = async (id) => {
+  console.log("Removing user " + id);
+  try {
+    const user = await db.one("DELETE FROM users WHERE id=$1 RETURNING *", id);
+    return user;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {};
