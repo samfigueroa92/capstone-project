09a17f1db6aa0fbd9cee382a3dd6a3831cff5fe8
Kalilah Clarke <thepursuit@Kalilahs-MBP.fios-router.home>
2022-11-19 14:25:58 -0500

    .env file added & google auth object included

--- front-end/src/Services/Firebase.js
+++ front-end/src/Services/Firebase.js
@@ -1,16 +1,17 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase.analytics';
-import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { GoogleAuthProvider , signInWithPopup, getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
-}
+  apiKey: "AIzaSyBYclzV7ojPwBUfzCWVAZZjpNioglYZpNc",
+  authDomain: "goldensolution-5327b.firebaseapp.com",
+  projectId: "goldensolution-5327b",
+  storageBucket: "goldensolution-5327b.appspot.com",
+  messagingSenderId: "1085334323375",
+  appId: "1:1085334323375:web:888f92775716009c14145a",
+  measurementId: "G-0MF7L0HSV1"
+};
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
