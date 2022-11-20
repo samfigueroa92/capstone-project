3aa05ff195ff47fa11675ae7fd8d974522e3ec29
Kalilah Clarke <thepursuit@Kalilahs-MBP.fios-router.home>
2022-11-19 21:19:01 -0500

    Senior Css

---
+++ front-end/src/Components/ProfileInfo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ProfileInfo = () => {
+  return (
+    <div>
+        <form>
+        <h3>General Information</h3>
+        <label>First Name: <input type="text"/></label>
+        <label>Last Name: <input type="text"/></label>
+        <label>Middle: <input type="text"/></label>
+
+        <h3>Address</h3>
+        <label>Address Line 1: <input type="text"/></label>
+        <label>Address Line 2: <input type="text"/></label>
+        <label>City: <input type="text"/></label>
+        <label>State: <select> </select></label>
+        <label>City: <input type="text"/></label>
+        <button>Next</button>
+        </form>
+    </div>
+  )
+}
+
+export default ProfileInfo
