61cb7e55cd2197608f0989ee26547099c44e35ee
Tom LaTulipe <tlatulipe@gmail.com>
2022-11-20 11:48:57 -0500

    see all and create ratings

--- back-end/queries/ratings.js
+++ back-end/queries/ratings.js
@@ -1,11 +1,29 @@
 const db = require("../db/dbConfig.js");
 
-//Index -- all users
+//Index -- all ratings
+const getRatings = async () => {
+  try {
+    console.log("Listing all ratings for dev purposes");
+    const ratings = await db.any("SELECT * FROM ratings ORDER BY id ASC");
+    return ratings;
+  } catch (error) {
+    return error;
+  }
+};
 
+// --Routes--
 
-
-
-
-
+// Create rating
+const leaveRating = async (rating) => {
+  try {
+    console.log("Adding rating to request");
+    rating = await db.one(
+      "INSERT INTO ratings (rating, request_id, rating_user, rated_user) VALUES ($1, $2, $3, $4) RETURNING *",
+      [rating.rating, rating.request_id, rating.rating_user, rating.rated_user]
+    );
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {};
