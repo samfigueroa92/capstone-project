ecc9dc1ff49dcf70dd64817923620b9c9e6a1480
Kalilah Clarke <thepursuit@Kalilahs-MBP.fios-router.home>
2022-11-19 22:02:03 -0500

    volunteer-page

--- front-end/src/Components/VolunteerPage.js
+++ front-end/src/Components/VolunteerPage.js
@@ -6,7 +6,8 @@ import Button from "react-bootstrap/Button";
 const VolunteerPage = () => {
   return (
     <div className="volunteer-page">
-      <h2>Volunteers</h2>
+      <div className ='volunteer-paragraph'>
+      <h2 className='volunteers'>Volunteers</h2>
       <div>
         <h5>What does a volunteer do?</h5>
         <p>
@@ -34,8 +35,9 @@ const VolunteerPage = () => {
           per conubia nostra, per inceptos himenaeos.
         </p>
       </div>
-      <Button>SIGN UP NOW</Button>
-      <img src="https://static.vecteezy.com/system/resources/previews/006/795/116/original/volunteer-man-walking-in-park-with-elderly-disabled-or-handicapped-woman-in-wheelchair-help-and-support-to-senior-people-with-health-problems-banner-for-5th-december-cartoon-illustration-vector.jpg" />
+      <Button className='button'>SIGN UP NOW</Button>
+      </div>
+      <img className ='volunteer-image'src="https://static.vecteezy.com/system/resources/previews/006/795/116/original/volunteer-man-walking-in-park-with-elderly-disabled-or-handicapped-woman-in-wheelchair-help-and-support-to-senior-people-with-health-problems-banner-for-5th-december-cartoon-illustration-vector.jpg" />
     </div>
   );
 };
