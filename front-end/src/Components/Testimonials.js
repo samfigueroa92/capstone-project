402fa1b5da903bae270ebba09843766390651357
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-11-20 12:38:04 -0500

    finish testimonials

--- front-end/src/Components/Testimonials.js
+++ front-end/src/Components/Testimonials.js
@@ -6,22 +6,22 @@ const Testimonials = () => {
             <h1>Testimonials</h1>
             <div className="testimonial-one">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSECqXIHtJOST_QdstjQolhhQ396OhZ4eQAUw&usqp=CAU" alt=""/>
-                <h6>Mela Stark</h6>
+                <h3>Mela Stark</h3>
                 <p>"Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services."</p>
             </div>
             <div className="testimonial-two">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUtFFVWqMfDDAAhXxcxxkNIl0I6CUsbqocSA&usqp=CAU" alt=""/>
-                <h6>Finn Styles</h6>
+                <h3>Finn Styles</h3>
                 <p>"Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services."</p>
             </div>
             <div className="testimonial-three">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNpSMMXKJY41wyVfASyOb4ritj9_tZPT43VA&usqp=CAU" alt=""/>
-                <h6>Bailey Cruz</h6>
+                <h3>Bailey Cruz</h3>
                 <p>"Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services."</p>
             </div>
             <div className="testimonial-four">
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTOcVt1LXMpKrBQrZVDFnjtaN9PZlC-UIrlg&usqp=CAU" alt=""/>
-                <h6>John Smith</h6>
+                <h3>John Smith</h3>
                 <p>"Testimonials are short quotes from people who love your brand. It's a great way to convince customers to try your services."</p>
             </div>
         </div>
