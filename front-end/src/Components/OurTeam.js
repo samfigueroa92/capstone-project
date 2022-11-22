f29affa7e76d3bbaf06683904e2a123f19f3013f
Kalilah Clarke <thepursuit@Kalilahs-MacBook-Pro.local>
2022-11-22 16:21:01 -0500

    SideBar Edits

--- front-end/src/Components/OurTeam.js
+++ front-end/src/Components/OurTeam.js
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import Footer from "./Footer";
 
 //CSS Imports
-import teamwork from '../images/teamwork.jpg'
 import "./OurTeam.css";
 
 const OurTeam = () => {
