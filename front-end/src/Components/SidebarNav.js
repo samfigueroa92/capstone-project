26d0e277866374fc49272c0bec6b349d3659bbfd
Kalilah Clarke <thepursuit@Kalilahs-MBP.fios-router.home>
2022-11-22 00:37:48 -0500

    SideBar Ternary Skeleton

--- front-end/src/Components/SidebarNav.js
+++ front-end/src/Components/SidebarNav.js
@@ -1,6 +1,26 @@
+import Calender from "./Calendar"
+import React, {useState} from "react";
 const SidebarNav = () => {
+    const [volunteer, setVolunteer] = useState(true)
     return (
-        <div></div>
+        <div className='SideBar'>
+            {volunteer ? 
+            <>
+            <p>Volunteer Dashboard</p>
+            <p>Accepted Requests</p>
+            <p>Open Requests</p>
+            <p>Achievements</p>
+            <p>Settings</p>
+            </>:
+            <>
+            <p>Senior Dashboard</p>
+            <p>My Requests</p>
+            <p>Volunteers</p>
+            <p>Submit A Request</p>
+            <p>Settings</p>
+            </>}
+            <Calender/>
+        </div>
     );
 };
 
