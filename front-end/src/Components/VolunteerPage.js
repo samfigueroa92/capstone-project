6afe14cbc1390061acb022a0b7be7b3a8063265e
Tom LaTulipe <tlatulipe@gmail.com>
2022-11-26 17:30:05 -0500

    removed duplicate code?

--- front-end/src/Components/VolunteerPage.js
+++ front-end/src/Components/VolunteerPage.js
@@ -55,33 +55,11 @@ const VolunteerPage = () => {
             className="volunteer-image"
             src="https://static.vecteezy.com/system/resources/previews/006/795/116/original/volunteer-man-walking-in-park-with-elderly-disabled-or-handicapped-woman-in-wheelchair-help-and-support-to-senior-people-with-health-problems-banner-for-5th-december-cartoon-illustration-vector.jpg"
           />
-        </div>
-        <div>
-          <h5>How do I apply?</h5>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed
-            ipsum at urna tincidunt pharetra ut a turpis. Vivamus vulputate
-            ligula in accumsan accumsan. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos.
-          </p>
-        </div>
-        <div>
-          <h5>What are the benefits?</h5>
-          <p>
-            orem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed
-            ipsum at urna tincidunt pharetra ut a turpis. Vivamus vulputate
-            ligula in accumsan accumsan. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos.
-          </p>
-        </div>
+        </div>        
         <Button className="button" onClick={() => navigate("/sign-up")}>
           SIGN UP NOW
         </Button>
-      </div>
-      <img
-        className="volunteer-image"
-        src="https://static.vecteezy.com/system/resources/previews/006/795/116/original/volunteer-man-walking-in-park-with-elderly-disabled-or-handicapped-woman-in-wheelchair-help-and-support-to-senior-people-with-health-problems-banner-for-5th-december-cartoon-illustration-vector.jpg"
-      />
+      </div>    
       <Footer />
     </>
   );
