43ec5b3e6fc80893873eeefeb99d62918d8a4a06
Tom LaTulipe <tlatulipe@gmail.com>
2022-11-26 17:20:22 -0500

    removed seemingly duplicate code

--- front-end/src/Components/SeniorsPage.js
+++ front-end/src/Components/SeniorsPage.js
@@ -12,69 +12,58 @@ const SeniorsPage = () => {
 
   return (
     <>
-    <div>
-      <div className="senior-page">
-        <div className="senior-paragraph">
-          <h2 className="seniors">Seniors</h2>
-          <div>
-            <h5>
-              <b>How does it work?</b>
-            </h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed
-              ipsum at urna tincidunt pharetra ut a turpis. Vivamus vulputate
-              ligula in accumsan accumsan. Class aptent taciti sociosqu ad
-              litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
-          </div>
-          <div>
-            <h5>
-              <b>How do I apply?</b>
-            </h5>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed
-              ipsum at urna tincidunt pharetra ut a turpis. Vivamus vulputate
-              ligula in accumsan accumsan. Class aptent taciti sociosqu ad
-              litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
-          </div>
-          <div>
-            <h5>
-              <b>What are the benefits?</b>
-            </h5>
-            <p>
-              orem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed
-              ipsum at urna tincidunt pharetra ut a turpis. Vivamus vulputate
-              ligula in accumsan accumsan. Class aptent taciti sociosqu ad
-              litora torquent per conubia nostra, per inceptos himenaeos.
-            </p>
-          </div>
-          {/* <Button className="button" onClick={() => setModalOpen(true)}>
+      <div>
+        <div className="senior-page">
+          <div className="senior-paragraph">
+            <h2 className="seniors">Seniors</h2>
+            <div>
+              <h5>
+                <b>How does it work?</b>
+              </h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                sed ipsum at urna tincidunt pharetra ut a turpis. Vivamus
+                vulputate ligula in accumsan accumsan. Class aptent taciti
+                sociosqu ad litora torquent per conubia nostra, per inceptos
+                himenaeos.
+              </p>
+            </div>
+            <div>
+              <h5>
+                <b>How do I apply?</b>
+              </h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                sed ipsum at urna tincidunt pharetra ut a turpis. Vivamus
+                vulputate ligula in accumsan accumsan. Class aptent taciti
+                sociosqu ad litora torquent per conubia nostra, per inceptos
+                himenaeos.
+              </p>
+            </div>
+            <div>
+              <h5>
+                <b>What are the benefits?</b>
+              </h5>
+              <p>
+                orem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+                sed ipsum at urna tincidunt pharetra ut a turpis. Vivamus
+                vulputate ligula in accumsan accumsan. Class aptent taciti
+                sociosqu ad litora torquent per conubia nostra, per inceptos
+                himenaeos.
+              </p>
+            </div>
+            {/* <Button className="button" onClick={() => setModalOpen(true)}>
             SIGN UP NOW
           </Button> */}
+          </div>
+          <img src="https://us.123rf.com/450wm/rumkavodki/rumkavodki2010/rumkavodki201000254/158148260-woman-helping-to-elderly-lady-with-shopping-cartoon-characters-of-volunteer-and-old-woman-at-decorat.jpg?ver=6" />
         </div>
-        <img src="https://us.123rf.com/450wm/rumkavodki/rumkavodki2010/rumkavodki201000254/158148260-woman-helping-to-elderly-lady-with-shopping-cartoon-characters-of-volunteer-and-old-woman-at-decorat.jpg?ver=6" />
+        <Button className="button" onClick={() => navigate("/sign-up")}>
+          SIGN UP NOW
+        </Button>
       </div>
-      <div>
-        <h5><b>How do I apply?</b></h5>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed
-          ipsum at urna tincidunt pharetra ut a turpis. Vivamus vulputate ligula
-          in accumsan accumsan. Class aptent taciti sociosqu ad litora torquent
-          per conubia nostra, per inceptos himenaeos.
-        </p>
-      </div>
-      <div>
-        <h5><b>What are the benefits?</b></h5>
-        <p>
-          orem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed
-          ipsum at urna tincidunt pharetra ut a turpis. Vivamus vulputate ligula
-          in accumsan accumsan. Class aptent taciti sociosqu ad litora torquent
-          per conubia nostra, per inceptos himenaeos.
-        </p>
-      </div>
-      <Button className='button' onClick={() => navigate("/sign-up")}>SIGN UP NOW</Button>
-    </div><img src="https://us.123rf.com/450wm/rumkavodki/rumkavodki2010/rumkavodki201000254/158148260-woman-helping-to-elderly-lady-with-shopping-cartoon-characters-of-volunteer-and-old-woman-at-decorat.jpg?ver=6" /><Footer /></>
+      <Footer />
+    </>
   );
 };
 
