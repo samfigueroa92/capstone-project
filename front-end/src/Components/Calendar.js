96ba966f68701b82d6dabb36822544bc128f16a6
Tom LaTulipe <tlatulipe@gmail.com>
2022-11-26 19:59:52 -0500

    calendar.js

---
+++ front-end/src/Components/Calendar.js
@@ -0,0 +1,14 @@
+//Component Import
+
+//CSS Import
+import "./Calendar.css"
+
+const Calendar = () => {
+    return (
+        <div className="calendar">
+
+        </div>
+    );
+};
+
+export default Calendar;
