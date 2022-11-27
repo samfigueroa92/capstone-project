bc238af53fed6e51239cdf2858d8c532647c5e37
sabrina <sabrinaescobarflores@gmail.com>
2022-11-27 10:29:54 -0500

    add to ProfileInfo

--- front-end/src/Components/ProfileInfo.js
+++ front-end/src/Components/ProfileInfo.js
@@ -1,9 +1,6 @@
-import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
-import "./ProfileInfo.css"
-import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const API = process.env.REACT_APP_API_URL;
 
@@ -16,74 +13,16 @@ const API = process.env.REACT_APP_API_URL;
 
 //Everything else should be visible and a button to Edit info
 
-function ProfileInfo () {
-  let { id } = useParams();
-  let navigate = useNavigate();
-
-  const states = {
-    'Alabama' : 'AL',
-    'Alaska' : 'AK',
-    'Arizona' : 'AZ',
-    'Arkansas' : 'AR',
-    'California' : 'CA',
-    'Canal Zone' : 'CZ',
-    'Colorado' : 'CO',
-    'Connecticut' : 'CT',
-    'Delaware' : 'DE',
-    'District of Columbia' : 'DC',
-    'Florida' : 'FL',
-    'Georgia' : 'GA',
-    'Guam' : 'GU',
-    'Hawaii' : 'HI',
-    'Idaho' : 'ID',
-    'Illinois' : 'IL',
-    'Indiana' : 'IN',
-    'Iowa' : 'IA',
-    'Kansas' : 'KS',
-    'Kentucky' : 'KY',
-    'Louisiana' : 'LA',
-    'Maine' : 'ME',
-    'Maryland' : 'MD',
-    'Massachusetts' : 'MA',
-    'Michigan' : 'MI',
-    'Minnesota' : 'MN',
-    'Mississippi' : 'MS',
-    'Missouri' : 'MO',
-    'Montana' : 'MT',
-    'Nebraska': 'NE',
-    'Nevada' : 'NV',
-    'New Hampshire' : 'NH',
-    'New Jersey' : 'NJ',
-    'New Mexico' : 'NM',
-    'New York' : 'NY',
-    'North Carolina' : 'NC',
-    'North Dakota' : 'ND',
-    'Ohio' : 'OH',
-    'Oklahoma' : 'OK',
-    'Oregon' : 'OR',
-    'Pennsylvania' : 'PA',
-    'Puerto Rico' : 'PR',
-    'Rhode Island': 'RI',
-    'South Carolina' : 'SC',
-    'South Dakota' : 'SD',
-    'Tennessee' : 'TN',
-    'Texas' : 'TX',
-    'Utah' : 'UT',
-    'Vermont' : 'VT',
-    'Virgin Islands' : 'VI',
-    'Virginia' : 'VA',
-    'Washington': 'WA',
-    'West Virginia' : 'WV',
-    'Wisconsin': 'WI',
-    'Wyoming': 'WY'
-    }
+const ProfileInfo = () => {
+  let { index } = useParams();
+  const navigate = useNavigate();
 //"dob", "address", "unit", "city", "zipcode", "phonenumber", "email", "profilephoto", "user_type", "state"
   const [user, setUser] = useState({
     firstname: "",
     lastname: "",
     dob: "",
     address: "",
-    unit: "",
+    unit: false,
     city: "",
     zipcode: "",
     phonenumber: "",
@@ -92,33 +31,29 @@ function ProfileInfo () {
     user_type: "",
     state: "",
   });
-//axios needs to be work on base on login model
-    const updateUser = (updatedUser) => {
-      axios
-      .put(`${API}/users/${id}`, updatedUser) 
-      .then(
-        () => {
-          navigate(`/users/${id}`);
-        },
-        (error) => console.error(error)
-      )
-      .catch((c) => console.warn("catch", c));
-    }
- 
+
   const handleTextChange = (event) => {
-    setUser({ ...user, [event.target.id]: event.target.value });
+    setBookmark({ ...user, [event.target.id]: event.target.value });
   };
 
   useEffect(() => {
-    axios.get(`${API}/users/${id}`)
+    axios.get(`${API}/users/${index}`)
       .then(response => setUser(response.data))
-      .catch((error) => console.log("not found")
-      );
-    }, [id, navigate]);
+      .catch(error => console.error(error))
+    }, []);
+
+    const updateUser = () => {
+      axios.put(`${API}/users/${index}`, user) 
+        .then(response => {
+          setUser(response.data)
+          navigate(`/users/${index}`)
+        })
+        .catch(error => console.error(error))
+    }
 
     const handleSubmit = (event) => {
       event.preventDefault();
-      updateUser(user, id);
+      updateUser();
     };
 
   return (
@@ -145,69 +80,55 @@ function ProfileInfo () {
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicFirstName">
               <Form.Label>First Name</Form.Label>
-              <Form.Control disabled
-              id="firstname"
-              value={user.firstname}
+              <Form.Control 
               type="text"
-              onChange={handleTextChange} 
+              value={user.firstname} 
               />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicLastName">
               <Form.Label>Last Name</Form.Label>
-              <Form.Control disabled
-              id="lastname"
+              <Form.Control 
+              type="text" 
               value={user.lastname}
-              type="text"
-              onChange={handleTextChange}  
               />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicDOB">
               <Form.Label>Date of Birth</Form.Label>
-              <Form.Control disabled
-              id="dob"
-              value={user.dob}
-              type="date"  
-              onChange={handleTextChange} 
+              <Form.Control 
+              type="date" 
+              value={user.dob} 
               />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control disabled
-              id="email"
-              value={user.email}
-              type="email"
-              onChange={handleTextChange} 
-              />
+              <Form.Control 
+              type="email" 
+              value={user.email}/>
               <Form.Text className="text-muted">
               </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicAddressLine1">
-              <Form.Label>Address</Form.Label>
-              <Form.Control 
-              id="address"
-              value={user.address}
-              type="text"
-              onChange={handleTextChange}
-              />
+              <Form.Label>Address Line 1</Form.Label>
+              <Form.Control type="text"/>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicAddressLine2">
+              <Form.Label>Address Line 2</Form.Label>
+              <Form.Control type="text" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicCity">
               <Form.Label>City</Form.Label>
-              <Form.Control
-              id="city" 
-              value={user.city}
-              type="text" 
-              onChange={handleTextChange} 
-              />
+              <Form.Control type="text"  />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicState">
               <Form.Label>State</Form.Label>
-              <Form.Select value={user.state} onChange={handleTextChange} >
+              <Form.Select>
                 <option>--Select State--</option>
                 {Object.values(states).map(state => <option>{state}</option>)}
               </Form.Select>
@@ -215,40 +136,41 @@ function ProfileInfo () {
 
             <Form.Group className="mb-3" controlId="formBasicZip">
               <Form.Label>Zip Code</Form.Label>
-              <Form.Control
-              id="zipcode" 
-              value={user.zipcode}
-              type="number"
-              onChange={handleTextChange} 
-              />
+              <Form.Control type="number"  />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPhone">
               <Form.Label>Phone Number</Form.Label>
-              <Form.Control 
-              id="phonenumber"
-              value={user.phonenumber}
-              type="tel" 
-              onChange={handleTextChange}
-              />
+              <Form.Control type="tel" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicVerificationType">
+              <Form.Label>Verification Type</Form.Label>
+              <Form.Select >
+                <option>--Select Option--</option>
+                <option>State ID</option>
+                <option>Passport</option>
+                <option>Drivers Liscense</option>
+              </Form.Select>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicSSI">
+              <Form.Label>Social Security Number</Form.Label>
+              <Form.Control type="hidden" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPhoto">
-              <Form.Label>Upload New Photo</Form.Label>
-              <Form.Control 
-              id="profilephoto"
-              value={user.profilephoto}
-              type="file"
-              onChange={handleTextChange} 
-              />
-              <Form.Text className="text-muted"> 
+              <Form.Label>Upload Photo</Form.Label>
+              <Form.Control type="file" placeholder="image" />
+              <Form.Text className="text-muted">
+              
               </Form.Text>
             </Form.Group>
-            <input type="submit" />
+
+            <Button variant="primary" type="submit">
+              Edit Info
+            </Button>
           </Form>
-          <Link to={`/users/${id}`}>
-        <button>Nevermind!</button>
-      </Link>
         </div>
       </Container>
     </div>
