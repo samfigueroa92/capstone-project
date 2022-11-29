6b76b4d8ebfe288a15581e819dc257c80684a127
Tom LaTulipe <tlatulipe@gmail.com>
2022-11-29 18:22:37 -0500

    Update seed.sql

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -9,6 +9,13 @@ INSERT INTO users ("uuid", "firstname", "lastname", "dob", "address", "unit", "c
 INSERT INTO requests ("elder_id", "volunteer_id", "req_date", "description", "location", "time", "assigned", "complete") VALUES
 ('14d3ba1b-b313-409e-ad4b-3a7e205d2a47','14d3ba1b-b313-409e-ad4b-3a7e205d2a89','2022-12-20', 'I need help with a few things around the house right before Christmas', 'Bronx', '12pm-3pm', 'FALSE','FALSE') RETURNING *;
 
+INSERT INTO requests ("elder_id", "volunteer_id", "req_date", "description", "location", "time", "assigned", "complete") VALUES
+('14d3ba1b-b313-409e-ad4b-3a7e205d2a47', '', '2022-12-20', 'Looking for some assistance is putting together an artificial Christmas tree','Brooklyn','12pm-5pm', 'FALSE','FALSE') RETURNING *;
+
+INSERT INTO requests ("elder_id", "volunteer_id", "req_date", "description", "location", "time", "assigned", "complete") VALUES
+('14d3ba1b-b313-409e-ad4b-3a7e205d2a47','','2022-12-06','','','','','') RETURNING *;
+
+
 INSERT INTO ratings ("rating","request_id","rating_user_id","rated_user_id") VALUES
 ('5','1','14d3ba1b-b313-409e-ad4b-3a7e205d2a89','14d3ba1b-b313-409e-ad4b-3a7e205d2a47') RETURNING *;
 
@@ -17,3 +24,8 @@ INSERT INTO reviews ("reviewer_id","description","post_date","request_id") VALUE
 
 INSERT INTO reviews ("reviewer_id","description","post_date","request_id") VALUES
 ('14d3ba1b-b313-409e-ad4b-3a7e205d2a47','TEST VOLUNTEER was a great help, I greatly appreciate their assistance','2022-12-04','1') RETURNING *;
+
+
+
+-- INSERT INTO requests ("elder_id", "volunteer_id", "req_date", "description", "location", "time", "assigned", "complete") VALUES
+-- ('','','','','','','','') RETURNING *;
