1726a1125f173ae38a32eaa304071a98d651d601
Tom LaTulipe <tlatulipe@gmail.com>
2022-11-29 19:19:24 -0500

    Update seed.sql

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -18,6 +18,11 @@ INSERT INTO requests ("elder_id", "volunteer_id", "req_date", "description", "lo
 INSERT INTO requests ("elder_id", "volunteer_id", "req_date", "description", "location", "time", "assigned", "complete") VALUES
 ('14d3ba1b-b313-409e-ad4b-3a7e205d2a47','','2022-12-10','A light in my closet blew, and I am unable to reach it. Looking for someone that could handle this small thing for me.','Bronx','10am','FALSE','FALSE') RETURNING *;
 
+INSERT INTO requests ("elder_id", "volunteer_id", "req_date", "description", "location", "time", "assigned", "complete") VALUES
+('14d3ba1b-b313-409e-ad4b-3a7e205d2a47','','2022-12-01','I need a few grocery items, but am currently recovering from knee surgery for a few more days. If someone would be kind enough to run this errand for me, I would be extremely grateful.','Queens','4pm','FALSE','FALSE') RETURNING *;
+
+INSERT INTO requests ("elder_id", "volunteer_id", "req_date", "description", "location", "time", "assigned", "complete") VALUES
+('14d3ba1b-b313-409e-ad4b-3a7e205d2a47','','2023-01-04','Looking for a little help','','','','') RETURNING *;
 
 INSERT INTO ratings ("rating","request_id","rating_user_id","rated_user_id") VALUES
 ('5','1','14d3ba1b-b313-409e-ad4b-3a7e205d2a89','14d3ba1b-b313-409e-ad4b-3a7e205d2a47') RETURNING *;
@@ -31,4 +36,4 @@ INSERT INTO reviews ("reviewer_id","description","post_date","request_id") VALUE
 
 
 -- INSERT INTO requests ("elder_id", "volunteer_id", "req_date", "description", "location", "time", "assigned", "complete") VALUES
--- ('','','','','','','','') RETURNING *;
+-- ('14d3ba1b-b313-409e-ad4b-3a7e205d2a47','','','','','','','') RETURNING *;
