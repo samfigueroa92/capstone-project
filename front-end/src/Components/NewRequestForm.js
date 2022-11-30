de76f04859250bb7ccb1e0da4592019ceb25b915
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-11-29 22:01:26 -0500

    adding to making a pull

--- front-end/src/Components/NewRequestForm.js
+++ front-end/src/Components/NewRequestForm.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const NewRequestForm = () => {
   return (
-    <div>NewRequestForm</div>
-  )
-}
+    <div>
+    
+    </div>
+  );
+};
 
 export default NewRequestForm;
