57c9cfcb89c53c16f6b5570c1dd4b3845f0760f5
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-11-30 09:15:16 -0500

    basic functionality of the form added

--- front-end/src/Components/NewRequestForm.js
+++ front-end/src/Components/NewRequestForm.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react"; 
+import { useNavigate } from "react-router-dom"; 
+import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
+
+// API 
+const API = process.env.REACT_APP_BACKEND_API_KEY;
+
 const NewRequestForm = () => {
+
+    let navigate = useNavigate();
+
+    // CREATE OR ADD A NEW REQUEST
+    const makeRequest = (newRequest) => {
+        axios.post(`${API}/requests`, newRequest)
+        .then(() => {
+            navigate(`/requests`)
+        },
+        (error) => console.error(error)
+        )
+        .catch((c) => console.warn("catch", c))
+    }
+
+    const [request, setRequest] = useState({
+        title: "",
+        req_date: "",
+        description: "",
+        location: "",
+        time: "", 
+        image: "",
+    });
   return (
     <div>
     
