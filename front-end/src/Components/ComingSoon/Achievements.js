7b5054842e7e3b870205a54d6546ad9bc97daa96
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-12-03 17:20:24 -0500

    coming soon page commenced

---
+++ front-end/src/Components/ComingSoon/Achievements.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./Achievements.css"
+
+const Achievements = () => {
+  return (
+    <div>Achievements</div>
+  )
+}
+
+export default Achievements;
---
+++ front-end/src/Components/ComingSoon/SvgIcon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SvgIcon = () => {
+  return (
+    <div>SvgIcon</div>
+  )
+}
+
+export default SvgIcon;
