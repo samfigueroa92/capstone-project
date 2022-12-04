6da0369a276257f6cb06b739f86dbbd6e400245e
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-12-04 15:39:30 -0500

    just took out the display of the photo and evelope from the mobile view

--- front-end/src/Components/NavBar/DashboardNav.js
+++ front-end/src/Components/NavBar/DashboardNav.js
@@ -13,31 +13,50 @@ import Dropdown from "react-bootstrap/Dropdown";
 //CSS Import
 import "./DashboardNav.css";
 
-const DashboardNav = ({applicationUser}) => {
+const DashboardNav = ({ applicationUser }) => {
   const user = useContext(UserContext);
   const navigate = useNavigate();
   const { displayName, photoURL } = user;
 
+  // const [click, setClick] = useState(false);
+
+  // methods
+  // const handleClick = () => setClick(!click);
+  // const closeMobileMenu = () => setClick(false);
+
   return (
-      <Navbar className="dash-navbar">
-        <Navbar.Brand href="/" className="nav-logo">
-          <img src="/images/logoGS.png" alt="logo" />
-        </Navbar.Brand>
-        <Container className="dashboard-nav">
+    <Navbar className="dash-navbar">
+      <Navbar.Brand href="/" className="nav-logo">
+        <img src="/images/logoGS.png" alt="logo" />
+      </Navbar.Brand>
+      <Container className="dashboard-nav">
+        {/* <div className="hidden"> */}
           <Nav className="me-auto.">
             <img src={photoURL} className="profile-pic" alt={displayName} />
+        
             <Dropdown>
-              <Dropdown.Toggle id="dropdown">{applicationUser.verified ? <i className="fa-solid fa-circle-check"></i> : null} {displayName.split(" ").shift()}</Dropdown.Toggle>
+              <Dropdown.Toggle id="dropdown">
+                {applicationUser.verified ? (
+                  <i className="fa-solid fa-circle-check"></i>
+                ) : null}{" "}
+                {displayName.split(" ").shift()}
+              </Dropdown.Toggle>
               <Dropdown.Menu>
-                <Dropdown.Item onClick={() => navigate("/user-dashboard")}>Dashboard</Dropdown.Item>
+                <Dropdown.Item onClick={() => navigate("/user-dashboard")}>
+                  Dashboard
+                </Dropdown.Item>
                 <Dropdown.Item onClick={signOut}>Sign-Out</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
+      
+
             <i className="fa-regular fa-envelope" id="envelope"></i>
             <i className="fa-regular fa-bell" id="bell"></i>
+          
           </Nav>
-        </Container>
-      </Navbar>
+        {/* </div> */}
+      </Container>
+    </Navbar>
   );
 };
 
