5ed0873debabf16842e67c5e5dd5e779993ec595
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-12-06 16:47:30 -0500

    back button added to the achievements page

--- front-end/src/Components/ComingSoon/Achievements.js
+++ front-end/src/Components/ComingSoon/Achievements.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, {useEffect} from "react";
+import { Link } from "react-router-dom";
 
 // Css
-import "./Achievements.css"
+import "./Achievements.css";
 
 // Components
 // import SidebarNav from "../Dashboard/SidebarNav.js";
-import SvgIcon from './SvgIcon';
+import SvgIcon from "./SvgIcon";
 
 const Achievements = () => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+  }, []);
   return (
-    <div className='achieve'  style={{paddingTop: "110px"}}>
-        {/* <SidebarNav /> */}
-        <SvgIcon />
-        <p>GoldenSolutions Achievements</p>
-        <p> Coming Soon!</p>
-        
+    <div className="achieve" style={{ paddingTop: "110px" }}>
+      <SvgIcon />
+      <p>GoldenSolutions Achievements</p>
+      <p> Coming Soon!</p>
+      <Link className="navigate" to="/user-dashboard">
+        BACK
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 export default Achievements;
