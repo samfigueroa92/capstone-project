98949427c01881af0e1529a10f079f96bf7d6a06
Kalilah Clarke <75289632+KalilahClarke@users.noreply.github.com>
2023-01-14 18:20:55 -0500

    Debugged MyRequest: removing setState for calendarDate and currentDate (#153)
    Co-authored-by: Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>

--- front-end/src/Components/Dashboard/MyRequests.js
+++ front-end/src/Components/Dashboard/MyRequests.js
@@ -8,56 +8,58 @@ import RequestCard from "./RequestCard";
 import "./MyRequests.css";
 
 const MyRequests = ({ requests, date, stringCurrentDate }) => {
-  const [value, setValue] = useState("");
-  const [currentDate, setCurrentDate] = useState("");
 
-  // useEffect(() => {
-  //   if (date) {
-  //     setCurrentDate(
-  //       stringCurrentDate.getFullYear() +
-  //         "-" +
-  //         ((stringCurrentDate.getMonth() + 1).toString().length === 1
-  //           ? "0" + (setCurrentDate.getMonth() + 1)
-  //           : stringCurrentDate.getMonth() + 1) +
-  //         "-" +
-  //         (stringCurrentDate.getDate().toString().length === 1
-  //           ? "0" + (stringCurrentDate.getDate())
-  //           : stringCurrentDate.getDate())
-  //     );
-  //     setValue(
-  //       date.getFullYear() +
-  //         "-" +
-  //         ((date.getMonth() + 1).toString().length === 1
-  //           ? "0" + (date.getMonth() + 1)
-  //           : date.getMonth() + 1) +
-  //         "-" +
-  //         (date.getDate().toString().length === 1
-  //           ? "0" + date.getDate()
-  //           : date.getDate())
-  //     );
-  //   }
-  // }, [date]);
+  let currentDate =
+    stringCurrentDate.getFullYear() +
+    "-" +
+    ((stringCurrentDate.getMonth() + 1).toString().length === 1
+      ? "0" + (stringCurrentDate.getMonth() + 1)
+      : stringCurrentDate.getMonth() + 1) +
+    "-" +
+    (stringCurrentDate.getDate().toString().length === 1
+      ? "0" + stringCurrentDate.getDate()
+      : stringCurrentDate.getDate());
   
 
-  const myspecifiedrequests = requests.length > 0 ? requests.map((request) => {
-    if (request.req_date === value) {
-      return <RequestCard key={request.id} request={request} />;
-    }
-  }) : <p className="no-requests">No accepted requests.</p>;
+  let calendarDate =
+    date?.getFullYear() +
+    "-" +
+    ((date?.getMonth() + 1).toString().length === 1
+      ? "0" + (date?.getMonth() + 1)
+      : date.getMonth() + 1) +
+    "-" +
+    (date?.getDate().toString().length === 1
+      ? "0" + date?.getDate()
+      : date?.getDate());
 
+  const myspecifiedrequests =
+    requests.length > 0 ? (
+      requests.map((request) => {
+        if (request.req_date === calendarDate) {
+          return <RequestCard key={request.id} request={request} />;
+        }
+      })
+    ) : (
+      <p className="no-requests">No accepted requests.</p>
+    );
 
-  const myrequests = requests.length > 0 ? requests.map((request) => {
-    if (request.req_date >= value) {
-      return <RequestCard key={request.id} request={request} />;
-    }
-  }) : <p className="no-requests">No accepted requests.</p>;
+  const myrequests =
+    requests.length > 0 ? (
+      requests.map((request) => {
+        if (request.req_date >= calendarDate) {
+          return <RequestCard key={request.id} request={request} />;
+        }
+      })
+    ) : (
+      <p className="no-requests">No accepted requests.</p>
+    );
 
   return (
     <>
-    <h3 className="head">My Requests</h3>
-    {/* <div className="my-requests">
-          {currentDate === value ? myrequests : myspecifiedrequests}
-    </div> */}
+      <h3 className="head">My Requests</h3>
+      <div className="my-requests">
+        {currentDate === calendarDate ? myrequests : myspecifiedrequests}
+      </div>
     </>
   );
 };
