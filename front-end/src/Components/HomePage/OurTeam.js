7d8a14df5332c97317e92a1bf6308e9e2a4b8fe4
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2023-03-10 02:17:09 -0500

    update ourteam page

--- front-end/src/Components/HomePage/OurTeam.js
+++ front-end/src/Components/HomePage/OurTeam.js
@@ -7,38 +7,13 @@ import "./OurTeam.css";
 const OurTeam = () => {
   const [team, setTeam] = useState([
     "samantha",
-    "tom",
-    "kalilah",
-    "adnan",
-    "sabrina",
-    "gigi",
-    "tim",
-    "amber",
   ]);
   const [learnMore, setLearnMore] = useState({
     sam: false,
-    tom: false,
-    kalilah: false,
-    adnan: false,
-    sabrina: false,
-    gigi: false,
-    tim: false,
-    amber: false,
   });
 
   const STAFFBIOS = {
-    sam: "I am a Nuyorican. While redesigning the website for my current employer I realized I had an interest in and enjoyed web development. After being referred to Pursuit by a friend, I decided it was time to bring my passion for helping others and interest in building useful and innovative websites into the world of tech.",
-    tom: "I’m a lifelong technophile, having been lucky enough to get a computer and NES into my home at the age of 2.  I’m glad to have found Pursuit, as the structure and pacing were exactly what I was looking for and failing to find through traditional school and personal study.",
-    kalilah:
-      "In school my favorite subject was Mathematics because I loved problem solving, I viewed math as a puzzle. I began my career as a Full-Stack Developer because of an Alumni Fellow who recognized those same qualities in web-development. I currently work as an Educator and Supervisor training others to optimally support and communicate with Middle School students  and though I love it, Pursuit has broken the ceiling as I pursue self development.",
-    adnan:
-      "’m from West Africa,  Accra Ghana. I moved here to pursue a lifelong dream of being a developer and help solve world problems through creativity and problem solving. I currently work as a storage manager. I began my journey as a full stack web-development with pursuit which has been a great experience with the skills I have acquired, which I will use in helping solve world problems through tech.",
-    sabrina:
-      "I am  currently a professional services’ Provider at the Mexican Consulate in New York and also I am studying Web Development in pursuit where I learned JavaScript, HTML, React, CSS and  Postgres. My biggest dream is to become a Web Developer and work for a big company where I can put my skills to use and gain more experience in coding.",
-    gigi: "Gigi,orem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed ipsum at urna tincidunt pharetra ut a turpis. Vivamus vulputate ligula in accumsan accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-    tim: "Tim,orem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed ipsum at urna tincidunt pharetra ut a turpis. Vivamus vulputate ligula in accumsan accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-    amber:
-      "Amber,orem ipsum dolor sit amet, consectetur adipiscing elit. Proin sed ipsum at urna tincidunt pharetra ut a turpis. Vivamus vulputate ligula in accumsan accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+    sam: "Hello, my name is Samantha. I currently work as the Adminstrative and Communications Associate for World Day of Prayer International. While redesigning their website I realized I had an interest in and enjoyed web development. After being referred to Pursuit by a friend, I decided it was time to bring my passion for helping others and interest in building useful and innovative websites into the world of tech. In my freetime I enjoy playing video games and learning how to roller skate.",
   };
 
   const handleLearnMore = (teamMate) => {
@@ -88,94 +63,8 @@ const OurTeam = () => {
             />
           </a>
           <p className="name"> Samantha Figueroa</p>
-          <p className="position"> President of Engineering</p>
+          <p className="position">Full-stack Web Developer</p>
           {handleLearnMore("sam")}
-        </div>
-        <div className="team">
-          <a href={`/our-page/${team[1]}`}>
-            <img
-              className="our-team-img"
-              src="https://media-exp1.licdn.com/dms/image/C4D03AQEUYyUqQXTZpQ/profile-displayphoto-shrink_800_800/0/1643244593566?e=1674691200&v=beta&t=mzXtv0JVf8sXvpzSPlT1YW-8aBDkFReJNLP3y6hxtNE"
-              alt="Tom LaTulipe"
-            />
-          </a>
-          <p className="name">Tom LaTulipe</p>
-          <p className="position"> VP of Engineering</p>
-          {handleLearnMore("tom")}
-        </div>
-        <div className="team">
-        <a href={`/our-page/${team[2]}`}>
-          <img
-            className="our-team-img"
-            src="https://media-exp1.licdn.com/dms/image/C5603AQFIoVA0ltC1Fg/profile-displayphoto-shrink_800_800/0/1592177861401?e=1674086400&v=beta&t=gGt0ISX9GlFHHDdsgcf4t97d5fpQs00mMEcWSSwQoV8"
-            alt="Kalilah Clarke"
-          />
-          </a>
-          <p className="name">Kalilah Clarke</p>
-          <p className="position">
-            Director of Engineering-User Facing Technologies
-          </p>
-          {handleLearnMore("kalilah")}
-        </div>
-        <div className="team">
-          <a href={`/our-page/${team[3]}`}>
-          <img
-            className="our-team-img"
-            src="https://ca.slack-edge.com/TCVA3PF24-U02LBBUQM39-804e6eb0aa9f-512"
-            alt="Adnan Abubakar Adams"
-            />
-            </a>
-          <p className="name">Adnan Abubakar Adams</p>
-          <p className="position">Senior Principal Engineer</p>
-          {handleLearnMore("adnan")}
-        </div>
-        <div className="team">
-        <a href={`/our-page/${team[4]}`}>
-          <img
-            className="our-team-img"
-            src="https://ca.slack-edge.com/TCVA3PF24-U02M4L7LAV7-07a20f756bae-512"
-            alt="Sabrina"
-          />
-          </a>
-          <p className="name">Sabrina Escobar Flores</p>
-          <p className="position">Mobile/Tablet Front-End Developer</p>
-          {handleLearnMore("sabrina")}
-        </div>
-        <div className="team">
-          <a href={`/our-page/${team[5]}`}>
-            <img
-              className="our-team-img"
-              src="https://ca.slack-edge.com/TCVA3PF24-U022L8PJX8B-edc76878813b-192"
-              alt="Gigi Scarsborough"
-            />
-          </a>
-          <p className="name">Gigi Scarborough</p>
-          <p className="position">Chief Technology Officer</p>
-          {handleLearnMore("gigi")}
-        </div>
-        <div className="team">
-          <a href={`/our-page/${team[6]}`}>
-            <img
-              className="our-team-img"
-              src="https://ca.slack-edge.com/TCVA3PF24-U022H04SBPF-6e9b68170b60-512"
-              alt="Tim McKiernan"
-            />
-          </a>
-          <p className="name">Tim McKiernan</p>
-          <p className="position">Chief Innovation Officer</p>
-          {handleLearnMore("tim")}
-        </div>
-        <div className="team">
-          <a href={`/our-page/${team[7]}`}>
-            <img
-              className="our-team-img"
-              src="https://ca.slack-edge.com/TCVA3PF24-U04CMRUKB6U-8ad816fd1907-512"
-              alt="Amber Bennet"
-            />
-          </a>
-          <p className="name">Amber Bennet</p>
-          <p className="position">Technical Consultant</p>
-          {handleLearnMore("amber")}
         </div>
       </div>
     </div>
