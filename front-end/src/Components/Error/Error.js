463b2ce5ab9aa9562d94942a0054319d20f447d6
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2023-07-18 01:00:45 -0400

    add page not found error

--- front-end/src/Components/Error/Error.js
+++ front-end/src/Components/Error/Error.js
@@ -1,16 +1,22 @@
-import './Error.css';
+import "./Error.css";
 
-const Error = () => {
-  
+const Error = ({ error }) => {
   return (
     <div className="Error">
       <img
         src="https://www.iconpacks.net/icons/1/free-error-icon-905-thumb.png"
         alt="error-icon"
       />
-      <div className="Error_content_header">Oops... something went wrong</div>
-      <div className="Error_content_text">
-        Error Message: Dummy Text
+      <div className="Error_content_header">
+        {error ? (
+          <>
+            <div className="Error_content_text">
+              Error Message: `${error.message}`
+            </div>
+          </>
+        ) : (
+          "Page Not Found"
+        )}
       </div>
     </div>
   );
